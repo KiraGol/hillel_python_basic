78d06722f6c7415d6646e5bea01e64b64f930765
KiraGol <kiragolovkova93@gmail.com>
2022-08-02 14:19:47 +0300

    first homework

---
+++ homework1/main.py
@@ -0,0 +1,13 @@
+# Shows the SyntaxError: not a chance
+from __future__ import braces
+
+# Imports a set of 19 rules for writing computer programs that influence
+# the design of the Python programming language (The Zen of Python).
+import this
+
+# Opened comics from the site: https://xkcd.com/353/
+import antigravity
+
+# Printed "Hello world!"
+import __hello__
+

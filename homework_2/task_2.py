81899c6571ade1ab3f0b6e3c81690149b8c4f4b4
KiraGol <kiragolovkova93@gmail.com>
2022-08-07 13:13:10 +0300

    second task

---
+++ homework_2/task_2.py
@@ -0,0 +1,3 @@
+entered_number = int(input(f"Please enter an integer number: "))
+print(f"Next number for number {entered_number} is {entered_number + 1}\n"
+      f"Previous number for number {entered_number} is {entered_number - 1}")

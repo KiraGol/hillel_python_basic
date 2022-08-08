59815a3513bb47a5cffb4aea611437aead735eff
KiraGol <kiragolovkova93@gmail.com>
2022-08-08 15:48:32 +0300

    task five

---
+++ homework_2/task_5.py
@@ -0,0 +1,7 @@
+number_x = int(input('Enter the number x: '))
+if number_x > 0:
+    print(f"sign({number_x}) = 1")
+elif number_x < 0:
+    print(f"sign({number_x}) = -1")
+else:
+    print(f"sign({number_x}) = 0")

44cd7e197a4a69533affa099395545b2f47b1abe
KiraGol <kiragolovkova93@gmail.com>
2022-08-17 22:06:40 +0300

    task seven in HW4

---
+++ homework_4/task_7.py
@@ -0,0 +1,10 @@
+list_1 = [1, 8, 9, 6, 5, 4, 5, 8, 1]
+list_2 = [6, 8, 9, 2, 3, 6]
+
+set_1 = set(list_1)
+set_2 = set(list_2)
+
+print(set_1.intersection(set_2))
+print(set_1.difference(set_2))
+print(set_1)
+print(set_2)

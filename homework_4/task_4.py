4917b048cc13b63a656d898f77bfdcb834196017
KiraGol <kiragolovkova93@gmail.com>
2022-08-20 21:41:24 +0300

    task four N2 HW4

--- homework_4/task_4.py
+++ homework_4/task_4.py
@@ -1,5 +1,6 @@
 num = str()
-for i in range(1, 10):
+user_input = int(input("Enter the height of the stairs: "))
+for i in range(1, user_input + 1):
     i = str(i)
     num = num + i
     print(num)

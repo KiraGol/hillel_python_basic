4eafa0c5b566e5949f44260de52d0c00ec89845a
KiraGol <kiragolovkova93@gmail.com>
2022-08-20 22:00:52 +0300

    task six N2 HW4

--- homework_4/task_6.py
+++ homework_4/task_6.py
@@ -1,5 +1,11 @@
 my_list = [72, 64, 32, 34, 82, 29, 59, 89, 32, 10, 41, 6, 7]
 
+elements_greater_than_two_neighbors = []
 for index in range(1, len(my_list) - 1):
     if my_list[index-1] < my_list[index] > my_list[index+1]:
-        print(index)
+        elements_greater_than_two_neighbors.append(index)
+
+print(f"Elements greater than two neighbors: "
+      f"{elements_greater_than_two_neighbors}")
+print(f"Count of elements that are greater than two neighbors: "
+      f"{len(elements_greater_than_two_neighbors)}")

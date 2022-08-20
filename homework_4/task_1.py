0f9646459831e0e6267062b4203bcfb1283de6f9
KiraGol <kiragolovkova93@gmail.com>
2022-08-20 11:56:43 +0300

    task one N2 HW1

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,7 +1,7 @@
-x = int(input("Enter number x: "))
-y = int(input("Enter number y: "))
+x = int(input("Enter how many kilometers the athlete ran on the first day: "))
+y = int(input("Enter how many kilometers the athlete ran in total: "))
 day = 1
 while y - x > 0:
     x = x + (x * 0.1)
     day += 1
-print(day)
+print(f"Number of the day on which the athlete ran the total distance: {day}")

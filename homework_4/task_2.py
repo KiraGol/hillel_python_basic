d6c2e261050dab438615959130753e946d40afe3
KiraGol <kiragolovkova93@gmail.com>
2022-08-20 12:09:20 +0300

    task two N2 HW4

--- homework_4/task_2.py
+++ homework_4/task_2.py
@@ -1,4 +1,5 @@
 import math
+
 user_input = None
 elements_to_operate = []
 while user_input != 0:
@@ -6,20 +7,20 @@ while user_input != 0:
     elements_to_operate.append(user_input)
 
 count_of_numbers = len(elements_to_operate) - 1
-print(count_of_numbers)
+print(f"Count of numbers: {count_of_numbers}")
 
 sum_of_numbers = sum(elements_to_operate)
-print(sum_of_numbers)
+print(f"Sum of numbers: {sum_of_numbers}")
 
 multiplication_of_numbers = math.prod(elements_to_operate)
-print(multiplication_of_numbers)
+print(f"Multiplication of numbers: {multiplication_of_numbers}")
 
-average_value_from_elements = sum_of_numbers/count_of_numbers
-print(int(average_value_from_elements))
+average_value_from_elements = float(sum_of_numbers / count_of_numbers)
+print(f"Average value from elements: {average_value_from_elements}")
 
 max_number = max(elements_to_operate)
 index_elem = elements_to_operate.index(max_number)
-print(max_number, index_elem + 1)
+print(f"Max. number: {max_number}, serial number: {index_elem + 1}")
 
 paired_elements = []
 unpaired_elements = []
@@ -30,28 +31,19 @@ for elem in elements_to_operate:
         unpaired_elements.append(elem)
 count_of_paired_elements = len(paired_elements)
 count_of_unpaired_elements = len(unpaired_elements)
-print(count_of_paired_elements)
-print(count_of_unpaired_elements)
+print(f"Count of paired elements: {count_of_paired_elements}")
+print(f"Count of unpaired elements: {count_of_unpaired_elements}")
 
 new_list = []
 for elem in elements_to_operate:
     if elem != max_number:
         new_list.append(elem)
 second_max_element = max(new_list)
-print(second_max_element)
+print(f"Second max element: {second_max_element}")
 
 new_list_2 = []
 for elem in elements_to_operate:
     if elem == max_number:
         new_list_2.append(elem)
 count_of_elements_equal_max_elem = len(new_list_2) - 1
-print(count_of_elements_equal_max_elem)
-
-
-
-
-
-
-
-
-
+print(f"Count of elements equal max elem: {count_of_elements_equal_max_elem}")

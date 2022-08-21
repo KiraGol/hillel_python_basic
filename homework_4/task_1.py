3ce9f53575c5d8b99c7c241a4a50a4cd98f202b6
KiraGol <kiragolovkova93@gmail.com>
2022-08-21 23:06:29 +0300

    task one N3 HW4

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,7 +1,8 @@
 x = int(input("Enter how many kilometers the athlete ran on the first day: "))
-y = int(input("Enter how many kilometers the athlete ran in total: "))
+y = int(input("Enter how many kilometers the athlete ran in searched day: "))
 day = 1
-while y - x > 0:
-    x = x + (x * 0.1)
+while x <= y:
+    x += x * 0.1
     day += 1
-print(f"Number of the day on which the athlete ran the total distance: {day}")
+print(f"Number of the day on which the athlete ran the new distance: {day}")
+

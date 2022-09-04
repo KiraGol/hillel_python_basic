4ec09cbb3ae896e2a18762f9adf904dd6e8a5f67
KiraGol <kiragolovkova93@gmail.com>
2022-09-04 20:45:34 +0300

    task 2 in HW7

---
+++ homework_7/task_2.py
@@ -0,0 +1,37 @@
+# 2. У додатку є json файл. Написати програму, яка прочитає його та порахує
+# загальну тривалість всіх треків в альбомі.
+#   Базовий кейс - поверне кількість секунд.
+#   * Дод. ускладнення повернути у вигляді рядка години:хвилини:секунди,
+#   прим. 0:41:39
+
+import json
+import time
+
+
+def get_data_from_json_file(name_of_json_file: str) -> dict:
+    """
+    get data from json file
+    :param name_of_json_file: str
+    :return: dict
+    """
+    with open(name_of_json_file, 'r', encoding='utf-8') as f:
+        data_from_json_file = json.load(f)
+        return data_from_json_file
+
+
+def get_duration_from_tracks(data_from_json_file: dict):
+    for key, value in data_from_json_file.items():
+        new_data = value
+        tracks = new_data.get('tracks')
+        for k, v in tracks.items():
+            new_new_data = v
+            duration = sum(int(n['duration']) for n in new_new_data)
+            return duration
+
+
+def conversion_to_time_format(duration: int):
+    time_format = time.strftime("%H:%M:%S", time.gmtime(duration))
+    return time_format
+
+
+print(conversion_to_time_format(get_duration_from_tracks(get_data_from_json_file("acdc.json"))))
